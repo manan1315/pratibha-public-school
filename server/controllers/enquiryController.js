409ecea3682643742738ef4dd4f90d0bddef8693
manan1315 <manan1315@gmail.com>
2026-08-29 14:12:00 +0530

    Fix enquiryController: restore missing CRUD exports

--- server/controllers/enquiryController.js
+++ server/controllers/enquiryController.js
@@ -1,8 +1,9 @@
+const Enquiry = require('../models/Enquiry');
+
 exports.submitEnquiry = async (req, res) => {
   try {
     const enquiry = await Enquiry.create(req.body);
     
-    // Send email only if email env vars are configured
     if (process.env.EMAIL_USER && process.env.EMAIL_PASS && process.env.EMAIL_HOST) {
       try {
         const { sendEmail } = require('../utils/sendEmail');
@@ -26,6 +27,33 @@ exports.submitEnquiry = async (req, res) => {
     }
 
     res.status(201).json({ message: 'Enquiry submitted successfully', enquiry });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getEnquiries = async (req, res) => {
+  try {
+    const enquiries = await Enquiry.find().sort({ createdAt: -1 });
+    res.json(enquiries);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateEnquiry = async (req, res) => {
+  try {
+    const enquiry = await Enquiry.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(enquiry);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteEnquiry = async (req, res) => {
+  try {
+    await Enquiry.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Enquiry deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
